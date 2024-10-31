fe36213e48943dd693f37064b6921f2ed5d546e9
JoeTCode <joejoe98t@gmail.com>
2024-10-31 15:33:23 +0000

    added onsubmit form redirect and reset fields button

--- client/src/components/AuctionCreation.jsx
+++ client/src/components/AuctionCreation.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const AUCTION_URL = import.meta.env.VITE_DEVELOPMENT_AUCTIONS_URL;
 
@@ -10,10 +11,19 @@ const AUCTION_URL = import.meta.env.VITE_DEVELOPMENT_AUCTIONS_URL;
 function AuctionCreation() {
 
   const user = useSelector((state) => state.user.user);
+  const navigate = useNavigate();
+
+  const formRef = useRef(); // Create a ref for the form
+
+  const resetAllFields = (e) => {
+    e.preventDefault();
+    formRef.current.reset(); // Reset the form using the ref
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
+    // for successful form submit redirect
+    
     // gets the form element
     const form = e.currentTarget;
     const data = new FormData(form);
@@ -38,14 +48,21 @@ function AuctionCreation() {
       .post(AUCTION_URL, entriesWithUID)
       .then((response) => {
         console.log(response.data);
-        resetAllFields();
+        console.log(response);
+        if (response.statusText === 'Created') {
+          navigate('/auctions')
+          
+        }
       })
       .catch((error) => {
         console.log(error);
-      });
+      })
+      
+
+      
   }
   return (
-    <form className='grid grid-rows-1 gap-4 mx-[40%]' onSubmit={handleSubmit}>
+    <form className='grid grid-rows-1 gap-4 mx-[40%]' onSubmit={handleSubmit} ref={formRef}>
         <h3> Create auction</h3>
         <label htmlFor='title'></label>
         <input type="text" id='title' name='title' />
@@ -57,7 +74,8 @@ function AuctionCreation() {
         <input type="file" id='file' name='file' />
         <label htmlFor='endDatetime'></label>
         <input type="datetime-local" id='end_time' name='end_time' />
-        <button type='submit' className='btn'>Create Auction</button>
+        <button type='submit' className='btn'> Create Auction </button>
+        <button onClick={resetAllFields}>Reset?</button>
     </form>
   )
 }
